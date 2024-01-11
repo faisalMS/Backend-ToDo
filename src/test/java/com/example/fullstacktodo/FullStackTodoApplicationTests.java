0c782dbc277e5f0dfae814c8fd19c921955abd90
FaisalMS <fis445@hotmail.com>
2024-01-11 23:38:44 +0300

    V1

---
+++ src/test/java/com/example/fullstacktodo/FullStackTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstacktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
